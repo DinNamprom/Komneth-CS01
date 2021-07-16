be320a2ac1354e6ba79a1fc12341c145f6a44b50
DinNamprom <56390732+DinNamprom@users.noreply.github.com>
2021-07-16 10:43:27 +0700

    Create CS01-Grading.py

---
+++ CS01-Grading.py
@@ -0,0 +1,20 @@
+A = int(input("Input 1:"))
+B = int(input("Input 2:"))
+C = int(input("Input 3:"))
+Score = A + B + C
+if 100 >= Score >= 80:
+    print("A")
+elif 80 > Score >= 75:
+    print("B+")
+elif 75 > Score >= 70:
+    print("B")
+elif 70 > Score >= 65:
+    print("C+")
+elif 65 > Score >= 60:
+    print("C")
+elif 60 > Score >= 55:
+    print("D+")
+elif 55 > Score >= 50:
+    print("D")
+elif 50 > Score >= 0:
+    print("F")
