31c8a20a78a691d1c11b0d26cb1e32b11f6aa5ba
DinNamprom <56390732+DinNamprom@users.noreply.github.com>
2021-08-06 09:08:24 +0700

    Create CS01-13.py
    while loop :P

---
+++ CS01-13.py
@@ -0,0 +1,11 @@
+lo = int(input("Enter loop: "))
+score_list = []
+a = 0
+for i in range(lo):
+    data = int(input("input score: "))
+    score_list += [data]
+while a <= lo:
+    if score_list[a] < 0:
+        break
+    print(score_list[a])
+    a += 1
