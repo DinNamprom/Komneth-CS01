0613eb0a2bbb1f28a22e7f1bfa908d327b9fa1ce
DinNamprom <56390732+DinNamprom@users.noreply.github.com>
2021-08-06 09:09:55 +0700

    Update CS01-13.py
    fix array

--- CS01-13.py
+++ CS01-13.py
@@ -4,7 +4,7 @@ a = 0
 for i in range(lo):
     data = int(input("input score: "))
     score_list += [data]
-while a <= lo:
+while a <= lo -1:
     if score_list[a] < 0:
         break
     print(score_list[a])
